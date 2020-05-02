fe5d2a142c27530af5f097592e4819662b5d7066
engrcollins <akinbamicollins@gmail.com>
2020-05-02 21:10:41 +0000

    Updates

---
+++ precache-manifest.d59296de9b19b20a57120fe0501c8a9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8364bfd3445e8f488dc05cad5eaa77",
+    "url": "/web-journal/index.html"
+  },
+  {
+    "revision": "5c8c18976a81fcdfaf36",
+    "url": "/web-journal/static/css/main.8aabdfbe.chunk.css"
+  },
+  {
+    "revision": "9309185ceec11227e9cf",
+    "url": "/web-journal/static/js/2.c2b78410.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-journal/static/js/2.c2b78410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8c18976a81fcdfaf36",
+    "url": "/web-journal/static/js/main.78b3b532.chunk.js"
+  },
+  {
+    "revision": "fd806d9bfc2245512d77",
+    "url": "/web-journal/static/js/runtime-main.cb98a187.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-journal/static/media/logo.5d5d9eef.svg"
+  }
+]);
