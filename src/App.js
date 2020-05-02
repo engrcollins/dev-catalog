f33bc1029b1a869c9827cdbb9fe2d28dae657288
engrcollins <akinbamicollins@gmail.com>
2020-05-02 22:20:41 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './Header'
+import ArchiveNav from './Navigation'
+import "./App.css";
+import AddCustomer from "./components/AddCustomer";
+import Customer from "./components/Customer";
+import CustomersList from "./components/CustomersList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="" style={{marginLeft: "20px"}} >
+      <Header />
+        <ArchiveNav />
+        <div className=" row" style={{textAlign: "center"}}>
+          <Router>
+            <div>
+              <ul className="nav nav-pills page-menu">
+                <li className="active"><a href="/customers" className="">
+                ARCHIVE TABLE</a></li>
+                <li><Link to={"/customers"} className="">
+                    Articles
+                  </Link></li>
+                  <li><Link to={"/add"} className="">
+                    New Article
+                  </Link></li>
+              </ul>
+              </div>
+              <br/>
+              <div className="appContent" style={{textAlign: "center"}}>
+                <Switch>
+                  <Route exact path={["/", "/customers"]} component={CustomersList} />
+                  <Route exact path="/add" component={AddCustomer} />
+                  <Route path="/customers/:id" component={Customer} />
+                </Switch>
+              </div>
+        </Router>
+        </div>
     </div>
   );
 }
