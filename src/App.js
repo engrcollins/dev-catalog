fb6cba82818834f1816a1a10e39bf3a9665fa4d5
engrcollins <akinbamicollins@gmail.com>
2020-05-05 01:42:22 +0000

    add more component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {component, useState} from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Container, Grid, Paper, Typography } from "@material-ui/core";
 import LayersIcon from '@material-ui/icons/Layers';
-import AddBoxIcon from '@material-ui/icons/AddBox';
 //import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './Header';
 import ArchiveNav from './Navigation';
