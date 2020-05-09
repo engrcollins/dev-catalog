b2d1423fbf416355f1412ab23995f550d0db78fb
engrcollins <akinbamicollins@gmail.com>
2020-05-09 18:03:45 +0000

    add some icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./App.css";
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import { Link } from "react-router-dom";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography} from "@material-ui/core";
+import EmailIcon from '@material-ui/icons/Email';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InfoIcon from '@material-ui/icons/Info';
+import LiveHelpIcon from '@material-ui/icons/LiveHelp';
+import FeaturedVideoIcon from '@material-ui/icons/FeaturedVideo';
+import PolicyIcon from '@material-ui/icons/Policy';
 
 /*var currentTime = new Date()
 var year = currentTime.getFullYear()
@@ -34,36 +40,45 @@ const Footer = () => {
     const classes = useStyles();
     return(
       <div id= "footer">
-        <Grid container spacing={3} className={classes.barback} position="static" padding ="4px" >
-            <Grid item xs={12} sm={4} style={{textAlign:"left", paddingLeft: "14px"}}>
+        <Grid container spacing={3} className={classes.barback} position="static" padding ="1px" >
+            <Grid item xs={12} sm={8} style={{textAlign:"left", paddingLeft: "14px"}}>
                 <Typography>
-                    <p><b>Contact us:</b>
-                        <br />
-                            Dev Journal
-                        <br />
-                            Mail: mccollinstechnologies@gmail.com.
-                        <p>Follow us on: <a href="https://twitter.com/engrcollins14" target="_blank">
-                            Twitter</a>, &nbsp;
-                            <a href="https://facebook.com/engrcollins14" target="_blank">Facebook </a>, &nbsp;
-                            <a href="https://qr.ae/TmZyQH" target="_blank">Quora </a>, &nbsp;
-                            <a href="https://medium.com" target="_blank">Medium.</a></p>
-                        </p>
-                </Typography>
-            </Grid>
-            <Grid item xs={12} sm={5} style={{padding:"3px", paddingTop:"65px", textAlign:"center"}} >
-                <Typography>
-                    <p> <a href="#">About Us</a> |  <a href="#">Terms of Service</a> |  <a href="#">Privacy Policy</a> |  <a href="#">FAQ</a> |  <a href="#">Advertise</a> </p>
-                    <p id="copyright"><b>Dev Journal &copy; 2019 - <span className="copyright"></span></b></p>
-                </Typography>
-            </Grid>
-            <Grid item xs={12} sm={3} style={{paddingTop:"25px", textAlign:"center"}}>
-                <Typography >
-                    <p><b>Disclaimer: </b>
+                    <p>
+                       <b>&lt;Dev /&gt; Journal</b> is a project of <b>McCollins Technologies.</b>
+                       <p><b>Disclaimer: </b>
                         All articles are referenced with source and author.
+                        </p>
                     </p>
+                        
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} style={{paddingTop:"2px", textAlign:"left"}}>
+                <Typography >
+                    <p>
+                        <b>Contact us:</b>
+                        <br/>
+                            <LocationOnIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/> McCollins Technologies
+                        <br/>
+                            <EmailIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/> mccollinstechnologies@gmail.com.
+                        <br/>
+                            Follow us on: <a href="https://twitter.com/engrcollins14" target="_blank">
+                            <TwitterIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/>Twitter,</a> &nbsp;
+                            <a href="https://facebook.com/engrcollins14" target="_blank"><FacebookIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/>Facebook, </a> &nbsp;
+                            <a href="https://qr.ae/TmZyQH" target="_blank">Quora, </a> &nbsp;
+                            <a href="https://medium.com" target="_blank">Medium.</a>
+                    </p> 
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} style={{padding:"1px", marginTop:"-15px", textAlign:"center"}} >
+                <Typography>
+                    <p>
+                        <a href="#"> <InfoIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/>About Us</a> &nbsp;|&nbsp; <a href="#"> <PolicyIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/>Privacy Policy</a> &nbsp;|&nbsp; <a href="#"><LiveHelpIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/>FAQ</a> &nbsp;|&nbsp; <a href="#"><FeaturedVideoIcon style={{ display: "inline-block", marginBottom:"-5px", fontSize:"20px" }}/>Advertise</a>
+                    </p>
+                    <p id="copyright"><b>McCollins Technologies &copy; 2019 - <span className="copyright"></span></b></p>
                 </Typography>
             </Grid>
         </Grid>
+
       </div>
       );
     }
