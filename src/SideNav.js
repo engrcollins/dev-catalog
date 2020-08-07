f1a8866b056d7b577beb60ea114d9a61f5d727f0
engrcollins <akinbamicollins@gmail.com>
2020-08-07 07:15:38 +0000

    fix bugs

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
